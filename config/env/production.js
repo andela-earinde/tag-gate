ae09cb4fb88cd29a9942c21832fc7e72574cbcef
Arinde Eniola <eniola.arinde@andela.co>
2015-04-23 17:11:35 +0100

    deploying to heroku

---
+++ config/env/production.js
@@ -0,0 +1,18 @@
+module.exports = {
+  url: {
+    signup: "https://user-tag.herokuapp.com/api/users/signup",
+    login: "https://user-tag.herokuapp.com/api/users/login",
+    remove: "https://user-tag.herokuapp.com/api/users/remove",
+    allUsers: "https://user-tag.herokuapp.com/api/users"
+  },
+
+  tagurl: {
+    allTags: "https://mean-tag.herokuapp.com/api/tags"
+  },
+
+  corsOptions: {
+    origin: "http://localhost:8000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }
+}
